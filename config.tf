2d62784e8c911cce4d8a7c567988505463bb55ea
pdobrinskiy <33158262+pdobrinskiy@users.noreply.github.com>
2021-09-16 10:13:24 +0300

    Update config.tf

--- config.tf
+++ config.tf
@@ -8,11 +8,8 @@ terraform {
 
 
 provider "aci" {
-# cisco-aci user name
-username = "admin"
-# cisco-aci password
-password = "test"
-# cisco-aci url
-url ="10.11.11.11"
-insecure = true
+  username = "admin"
+  password = "!v3G@!4@Y"
+  url      = "https://sandboxapicdc.cisco.com"
+  insecure = true
 }
